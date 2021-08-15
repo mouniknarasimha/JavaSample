f6a38dc05a338f30450a3e2860d35eeeea1df64a
Mounik Narasimha <mouniknarasimha@gmail.com>
2021-08-15 18:29:01 +0530

    initial code

---
+++ src/hello.java
@@ -0,0 +1,16 @@
+public class hello {
+
+    public static void main(String[] args){
+        System.out.println("hello nani");
+
+        int a,b;
+        a=5;
+        b=a*5;
+        int temp=b-a;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(temp);
+
+
+    }
+}
